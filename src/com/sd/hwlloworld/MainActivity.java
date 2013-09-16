51eea696355837c6f93c9267ff9aacdeb5abc75a
kukei <quke@supdo.com>
2013-09-16 17:24:30 +0800

    添加了输入框和按钮。
    Signed-off-by: kukei <quke@supdo.com>

--- src/com/sd/hwlloworld/MainActivity.java
+++ src/com/sd/hwlloworld/MainActivity.java
@@ -2,14 +2,45 @@ package com.sd.hwlloworld;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.res.Resources;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends Activity {
+	
+	private EditText etUserName, etPassWord;
+	private Button btnLogin;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		this.etUserName = (EditText)findViewById(R.id.etUserName);
+		this.etPassWord = (EditText)findViewById(R.id.etPassWord);
+		this.btnLogin = (Button)findViewById(R.id.btnLogin);
+		
+		this.btnLogin.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				if(etUserName.getText().toString().length()==0 || etPassWord.getText().toString().length()==0){
+                    //Resources res = getResources();
+                    new AlertDialog.Builder(MainActivity.this)
+                            .setTitle("提示")
+                            .setMessage("不能为空！")
+                            .setPositiveButton("确定", null)
+                            .show();
+                }else{
+                    
+                }
+			}
+		});
+		
 	}
 
 	@Override
